639d91f7be150648849e90e0f73a53d0eab56032
ilyasjafarli <ilyas.cefer@gmail.com>
2023-09-29 14:47:25 +0200

    3rd commit

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,28 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Homework A");
+Console.WriteLine("Homework A"); //1
+
+
+Console.WriteLine("Iki eded daxil edin:"); //1
+int eded1 = Convert.ToInt32(Console.ReadLine()); //1
+int eded2 = Convert.ToInt32(Console.ReadLine()); //1
+
+while (eded1 > eded2) // n
+{
+    Console.WriteLine("Birinci eded ikinci ededden kicik olmalidir"); //n
+    Console.Write("Birinci ededi yazin: "); //n
+    eded1 = Convert.ToInt32(Console.ReadLine()); //n
+    Console.WriteLine("Ikinci ededi yazin: "); //n
+    eded2 = Convert.ToInt32(Console.ReadLine());    //n
+} 
+
+    int sum = 0; //1
+while (eded1 <= eded2) //n
+{
+    sum += eded1; // n
+    eded1++; //n
+} 
+//On
+        Console.WriteLine("Cavab: " + sum); //1
 
 /*
-Console.WriteLine("Iki eded daxil edin:");
-
-int eded1 = Convert.ToInt32(Console.ReadLine());
-int eded2 = Convert.ToInt32(Console.ReadLine());
-
-    int eded3 = 0;
-if (eded1 > eded2)
-{
-    Console.WriteLine("Birinci eded ikinci ededden kicik olmalidir");
-}
-else
-{
-    int[] nums = new int[eded3];
-    int sum = 0;
-    for (int i = eded1; i < eded2; i++)
-    {
-        sum += nums[i];
-    }
-        Console.WriteLine($"{sum}");
-} */
-
 Console.WriteLine("Homework B");
 
 Console.WriteLine("Nece ededin ortalamasini tapmaq isteyirsiniz?");
@@ -39,4 +41,4 @@ for (int j = 0; j < b; j++)
 Console.WriteLine($"Cavab: {sum/=b}");
 
 Console.WriteLine("Homework C");
-
+*/
